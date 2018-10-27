61022f7b00b1726ab2a34bce57f4f85f71ca5b2d
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2018-10-27 11:04:08 +0530

    my lunch first commit

---
+++ mylunch/domain/partials/partial-admin-uom-all/script.js
@@ -0,0 +1,35 @@
+WEBDOCK.component().register(function(exports){
+    var scope;
+
+    function loadProductCategories(skip, take){
+        var handler = exports.getComponent("uom-handler");
+        
+        handler.transformers.allUom()
+        .then(function(result){
+            scope.items = result.result;
+        })
+        .error(function(){
+    
+        });
+    }
+
+    var vueData =  {
+        methods:{
+            navigatePage: function(){
+
+            }
+        },
+        data :{
+            items : []
+        },
+        onReady: function(s){
+            scope = s;
+            loadProductCategories(0,100);
+            $("body").css("background-image","none");
+        }
+    } 
+
+    exports.vue = vueData;
+    exports.onReady = function(){
+    }
+});
---
+++ mylunch/domain/partials/partial-cart-paycomplete/script.js
@@ -0,0 +1,18 @@
+WEBDOCK.component().register(function(exports){
+    var scope;
+    var bindData = {deliverToday:false};
+    var vueData =  {
+       data : bindData,
+       onReady: function(){
+            bindData.deliverToday = localStorage.deliverToday === 'true';
+            localStorage.removeItem("deliverToday");
+
+       }
+    }
+    
+
+    exports.vue = vueData;
+    exports.onReady = function(){
+    }
+});
+
